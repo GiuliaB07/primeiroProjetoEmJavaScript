2c1eda38a37620e724f7cb4b2038ce6e00bd1969
Giulia Bianchi da Silva <insomnia070406@gmail.com>
2026-08-15 18:26:12 -0300

    definindo o conteúdo do elemento como a mensagem

--- primeiroProjeto.js
+++ primeiroProjeto.js
@@ -3,3 +3,6 @@ const mensagemElemento = document.getElementById('resultado');
 
 //Define a mensagem que será exibida.
 const mensagem = 'Hello World!';
+
+//Define o conteúdo do elemento como a mensagem.
+mensagemElemento.textContent = mensagem;
