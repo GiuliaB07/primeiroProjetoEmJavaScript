0e98f48e50d0d16a73cee440c90807776dcd680f
Giulia Bianchi da Silva <insomnia070406@gmail.com>
2026-08-15 18:24:22 -0300

    mensagem e arrumação de pontuação

--- primeiroProjeto.js
+++ primeiroProjeto.js
@@ -1,2 +1,5 @@
 //Obtém o elemento com o ID "mensagem"
-const mensagemElemento = document.getElementById('resultado')
+const mensagemElemento = document.getElementById('resultado');
+
+//Define a mensagem que será exibida.
+const mensagem = 'Hello World!';
